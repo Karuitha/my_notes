ed9fe063a97d3bb2a7f4fffb98b2ba2cb3749a8a
Karuitha <diakingathia2005@gmail.com>
2021-11-05 22:33:31 +0300

    First commit of the notes

---
+++ BCOM 430 Mgt of financial instns/book.php
@@ -0,0 +1,399 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="pdf.css" type="text/css" />
+<LINK href="favicon.ico" rel="shortcut icon">
+
+
+<title>Download eBooks Free PDF Book Money and Banking online Book</title>
+<META name=description content="free books Money and Banking ebook Money and Banking  download pdf books free online Money and BankingebooksMoney and Banking download text book in pdf format textbook print e-book">
+<META name=keywords content="free books Money and Banking,free book ,,Money and Banking,pdf book on Money and Banking,online book on Money and Banking,download pdf ebook Money and Banking, online pdf books Money and Banking">
+</head>
+<body leftmargin="2" rightmargin="2" topmargin="0" bottommargin="0" align="center">
+
+<table id="main" class="main">
+  <tr>
+    <td id="main_t" class="main_t"><table width="995" height="90" style="border-collapse:collapse"  border="0" id="header_table">
+<tr>
+
+<td align="center" valign="middle" style="height: 1px">
+<a href="http://www.zainbooks.com" target="_parent">
+<img src="images/logo.gif" border="0"height="90"></a></td>
+
+<td width="728" align="center" valign="top" style="height: 1px">
+
+<script type="text/javascript"><!--
+google_ad_client = "pub-5148400695499158";
+/* PDF_TOP_728_90 */
+google_ad_slot = "1994823250";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></td>
+</tr>
+<tr>
+
+  <td colspan="2" align="center" valign="middle" style="height: 20px" bgcolor="#003366">
+  
+  <script type="text/javascript"><!--
+google_ad_client = "pub-5148400695499158";
+/* PDF_TOP_LUNIT_728_15 */
+google_ad_slot = "1774471998";
+google_ad_width = 728;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+  
+  </td>
+  </tr>
+</table></td>
+  </tr>
+  <tr>
+    <td id="main_m" class="main_m">
+	
+	<table id="mid" class="mid">
+      <tr>
+        <td id="mid_l" class="mid_l"><div align="right">
+<form action="http://www.google.com/cse" id="cse-search-box">
+  <div>
+    <input type="text" name="q" size="28" /><br />
+    <input type="hidden" name="cx" value="partner-pub-5148400695499158:iqrzl3-ixhq" />
+    <input type="hidden" name="ie" value="ISO-8859-1" />
+    <input type="submit" name="sa" value="Search" />
+  </div>
+</form>
+<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&amp;lang=en"></script>
+<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&amp;lang=en"></script>
+</div>
+<div align="right">
+<table width="200" id="navigation" style="border-collapse:collapse" border="0">
+      
+	  <tr>
+        <td height="10"></td>
+      </tr>
+	  
+	  <tr>
+        <td height="20">
+		<a href="http://www.zainbooks.com" class="navText"><strong>Home</strong></a></td>
+      </tr>
+	  <tr>
+        <td height="20">
+		<a href="http://www.zainbooks.com/blog" class="navText"><strong>Students Blog</strong></a></td>
+      </tr>
+
+      <tr>
+        <td height="20">&nbsp;</td>
+      </tr>
+
+
+ 
+<tr><td height="20"><a href="category.php?cat=arts"  class="navText">Arts</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=business"  class="navText">Business</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=commerce"  class="navText">Commerce</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=computer_science"  class="navText">Computer Science</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=earth_sciences"  class="navText">Earth Sciences</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=engineering"  class="navText">Engineering</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=exams_and_certifications"  class="navText">Exams and Certifications</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=formal_sciences"  class="navText">Formal Sciences</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=health_sciences"  class="navText">Health Sciences</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=humanities"  class="navText">Humanities</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=management"  class="navText">Management</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=mass_communication"  class="navText">Mass Communication</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=natural_sciences"  class="navText">Natural Sciences</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=political_science"  class="navText">Political Science</a></tr>
+ 
+<tr><td height="20"><a href="category.php?cat=social_sciences"  class="navText">Social Sciences</a></tr>
+ 
+
+      <tr>
+        <td height="20">&nbsp;</td>
+      </tr>
+
+	  <tr>
+        <td height="20">
+	<a href="sitemap.php" class="navText"><strong>SiteMap</strong></a></td>
+      </tr>
+
+	  <tr>
+        <td height="20">
+		<a href="http://www.zainbooks.com/catalog.html" class="navText"><strong>Classic Version</strong></a></td>
+      </tr>
+	  
+	  <tr>
+        <td height="20">
+
+		<a href="http://www.zainbooks.com/links.php" class="navText"><strong>Links</strong></a></td>
+      </tr>
+
+	  <tr>
+        <td height="20">
+
+		<a href="http://www.zainbooks.com/contact.html" class="navText"><strong>Contact Us</strong></a></td>
+      </tr>
+      
+    </table>
+</div>
+
+
+<br><br>
+
+<div align="left">
+
+<a href="http://www.mygeotv.com" class="latest">geo tv live</a>
+<br>
+<a href="http://www.web-vista.com/?cat=3" class="latest">free wallpapers</a>
+<br>
+
+<h1>Latest Additions</h1><ul><li><a href="book.php?elementary_linear_algebra_student_solutions_manual&id=256" class="latest">Elementary Linear Algebra Student Solutions Manual</a><br>
+<li><a href="book.php?elementary_linear_algebra&id=255" class="latest">Elementary Linear Algebra</a><br>
+<li><a href="book.php?op_amps_for_everyone&id=254" class="latest">Op Amps For Everyone</a><br>
+<li><a href="book.php?electronic_devices_conventional_current_version&id=253" class="latest">Electronic Devices Conventional Current Version</a><br>
+<li><a href="book.php?the_gmat_for_dummies&id=252" class="latest">The GMAT For Dummies</a><br>
+<li><a href="book.php?basic_electronics_math&id=251" class="latest">Basic Electronics Math</a><br>
+<li><a href="book.php?gre_general_test_practice_book&id=250" class="latest">GRE General Test Practice Book</a><br>
+<li><a href="book.php?gre_psychology_test_practice_book&id=249" class="latest">GRE Psychology Test Practice Book</a><br>
+<li><a href="book.php?gre_biochemistry,_cell_and_molecular_biology_test_practice_book&id=248" class="latest">GRE Biochemistry, Cell and Molecular Biology Test Practice Book</a><br>
+<li><a href="book.php?gre_biology_test_practice_book&id=247" class="latest">GRE Biology Test Practice Book</a><br>
+</ul><h1>Free Books</h1><ul><li><a href="book.php?the_picture_of_dorian_gray&id=211" class="latest">The Picture of Dorian Gray</a><br>
+<li><a href="book.php?common_mistakes_at_ietls_intermediate&id=140" class="latest">Common Mistakes at IETLS Intermediate</a><br>
+<li><a href="book.php?introduction_to_mass_communication&id=78" class="latest">Introduction to Mass Communication</a><br>
+<li><a href="book.php?how_to_understand_sculpture&id=118" class="latest">HOW TO UNDERSTAND SCULPTURE</a><br>
+<li><a href="book.php?woman_as_decoration&id=116" class="latest">Woman as Decoration</a><br>
+<li><a href="book.php?software_engineering_-_ii&id=17" class="latest">Software Engineering - II</a><br>
+<li><a href="book.php?human_resource_development&id=32" class="latest">Human Resource Development</a><br>
+<li><a href="book.php?fundamentals_of_auditing&id=25" class="latest">Fundamentals of Auditing</a><br>
+<li><a href="book.php?calculus_and_analytical_geometry_-_volume-1&id=62" class="latest">Calculus And Analytical Geometry - Volume-1</a><br>
+<li><a href="book.php?vanishing_england&id=111" class="latest">Vanishing England</a><br>
+<li><a href="book.php?cost_and_management_accounting&id=42" class="latest">Cost and Management Accounting</a><br>
+<li><a href="book.php?beyond_good_and_evil&id=172" class="latest">Beyond Good and Evil</a><br>
+<li><a href="book.php?chemistry&id=121" class="latest">Chemistry</a><br>
+<li><a href="book.php?macro_economics&id=69" class="latest">Macro economics</a><br>
+<li><a href="book.php?gre_general_test_practice_book&id=250" class="latest">GRE General Test Practice Book</a><br>
+<li><a href="book.php?great_expectations&id=179" class="latest">Great Expectations</a><br>
+<li><a href="book.php?pen_drawing_-_an_illustrated_treatise&id=113" class="latest">Pen Drawing - An Illustrated Treatise</a><br>
+<li><a href="book.php?micro_economics&id=70" class="latest">Micro economics</a><br>
+<li><a href="book.php?the_metamorphosis&id=209" class="latest">The Metamorphosis</a><br>
+<li><a href="book.php?forensic_psychology&id=87" class="latest">Forensic Psychology</a><br>
+<li><a href="book.php?asterisk:_the_future_of_telephony&id=239" class="latest">Asterisk: The Future of Telephony</a><br>
+<li><a href="book.php?oliver_twist&id=191" class="latest">Oliver Twist</a><br>
+<li><a href="book.php?gender_issues_in_psychology&id=88" class="latest">Gender Issues in Psychology</a><br>
+<li><a href="book.php?chemical_dynamics&id=120" class="latest">CHEMICAL DYNAMICS</a><br>
+<li><a href="book.php?barron's_gre_graduate_record_examination_2008&id=147" class="latest">Barron's GRE Graduate Record Examination 2008</a><br>
+<li><a href="book.php?gre_psychology_test_practice_book&id=249" class="latest">GRE Psychology Test Practice Book</a><br>
+<li><a href="book.php?the_poetry_of_architecture&id=110" class="latest">The Poetry of Architecture</a><br>
+<li><a href="book.php?erewhon_or_over_the_range&id=176" class="latest">Erewhon Or Over the Range</a><br>
+<li><a href="book.php?calculus&id=102" class="latest">CALCULUS</a><br>
+<li><a href="book.php?english_pronunciation_in_use&id=135" class="latest">English Pronunciation in Use</a><br>
+</ul></div></td>
+        <td id="mid_c" class="mid_c">
+		<table id="cenv" class="cenv">
+          <tr>
+            <td id="cenv_t" class="cenv_t"></td>
+          </tr>
+          <tr>
+            <td id="cenv_m" class="cenv_m">
+			<table id="cenh" class="cenh">
+              <tr>
+                <td id="cenh_l" class="cenh_l"></td>
+                <td id="cenh_c" class="cenh_c">                  
+				
+				<div align="center">
+				<table id="info" class="info">
+                    
+                    <tr><td id="info_1" class="info_1"><h1>Free Books Free PDF Books Download pdf eBooks Free Online ebook download</h1></td></tr>
+                    <tr><td id="info_2" class="info_2"><div align="center"><script type="text/javascript"><!--
+google_ad_client = "pub-5148400695499158";
+/* PDF_SQ_336_280 */
+google_ad_slot = "7334579097";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></div></td></tr>
+                    <tr><td id="info_3" class="info_3">                      
+					  
+<div align="left">				 
+
+<!----------------------- 1st Category ------------------->
+<a href="index.php" class="cat">Home</a> >>
+<a href="category.php?cat=commerce" class="cat">Commerce</a>
+ >> <a href="category.php?cat=banking" class="cat">Banking</a>
+ <a href="category.php?cat=" class="cat"></a>
+<br>
+<!----------------------- 2nd Category ------------------->
+<!----------------------- 3rd Category ------------------->
+
+<br>
+</div>
+
+<div align="center">
+					  <table  class="book_info" id="book_info">
+					  
+					  <tr><td id="book_info_h" class="book_info_h" colspan="2">
+					  <h1 class="a">Money and Banking</h1>
+					  </td></tr>
+
+<tr>
+                        <td  class="book_info_l" id="book_info_l">Title:</td>
+                        <td  class="book_info_r" id="book_info_r">Money and Banking</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Author:</td>
+                        <td class="book_info_r"></td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">ISBN:</td>
+                        <td class="book_info_r"></td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Publisher: </td>
+                        <td class="book_info_r"></td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Year: </td>
+                        <td class="book_info_r">2001</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Edition: </td>
+                        <td class="book_info_r">1</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Pages: </td>
+                        <td class="book_info_r">146</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Format: </td>
+                        <td class="book_info_r">pdf</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">File Size: </td>
+                        <td class="book_info_r">680 KB</td>
+                      </tr>
+                      <tr>
+                        <td class="book_info_l">Read</td>
+
+<td class="book_info_r">
+<a href="display.php?id=jh6fc127mt7gj8s51k5ha22in4nl3" class="book_link">Money and Banking</a>
+</td>
+</tr>
+<tr>
+<td class="book_info_l">Download</td>
+<td class="book_info_r">
+<a href="display.php?id=jh6fc127mt7gj8s51k5ha22in4nl3" class="book_link">Money and Banking</a>
+</td>
+</tr>
+                      
+                        
+                        </table></div>
+ 
+
+<table><tr><td>
+<h1 class="r">Related Books</h1><div align="left"><ul>
+<li><a href="book.php?financial_accounting&id=23" class="books_link">Financial Accounting</a></li>
+<li><a href="book.php?e-commerce&id=10" class="books_link">E-Commerce</a></li>
+<li><a href="book.php?organization_development&id=52" class="books_link">Organization Development</a></li>
+<li><a href="book.php?fundamentals_of_auditing&id=25" class="books_link">Fundamentals of Auditing</a></li>
+<li><a href="book.php?financial_management&id=44" class="books_link">Financial Management</a></li>
+<li><a href="book.php?financial_statement_analysis&id=24" class="books_link">Financial Statement Analysis</a></li>
+<li><a href="book.php?money_and_banking&id=51" class="books_link">Money and Banking</a></li>
+<li><a href="book.php?investment_analysis_and_portfolio_management&id=26" class="books_link">Investment Analysis and Portfolio Management</a></li>
+<li><a href="book.php?taxation_managment&id=27" class="books_link">Taxation Managment</a></li>
+<li><a href="book.php?advanced_financial_accounting&id=21" class="books_link">Advanced Financial Accounting</a></li>
+</ul>
+
+</div>
+</td></tr> </table>
+						
+</td></tr>
+</table>
+				
+				</td><td id="cenh_r" class="cenh_r"></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td id="cenv_b" class="cenv_b"></td>
+          </tr>
+        </table>
+		</td>
+        <td id="mid_r" class="mid_r"><!-- AddThis Button BEGIN --><div class="addthis_toolbox addthis_default_style"><a href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4c23552f41b2b56a" class="addthis_button_compact">Share</a>
+<span class="addthis_separator">|</span><a class="addthis_button_facebook"></a><a class="addthis_button_myspace"></a><a class="addthis_button_google"></a><a class="addthis_button_twitter"></a></div><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4c23552f41b2b56a"></script><!-- AddThis Button END -->
+
+
+<div id="google_translate_element"></div><script> 
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en'
+  }, 'google_translate_element');
+}
+</script><script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+<script type="text/javascript"><!--
+google_ad_client = "pub-5148400695499158";
+/* PDF_R_160_600 */
+google_ad_slot = "9697483053";
+google_ad_width = 160;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> 
+</script>
+<br />
+<script type="text/javascript"><!--
+google_ad_client = "pub-5148400695499158";
+/* PDF_R_160_600 */
+google_ad_slot = "9697483053";
+google_ad_width = 160;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> 
+</script></td>
+      </tr>
+    </table>
+	
+	</td>
+  </tr>
+  <tr>
+    <td  id="main_b" class="main_b"><p>&nbsp;</p>
+<p><a href="http://www.zainbooks.com" class="latest"><strong>Free Books</strong></a> | <a href="http://www.zainbooks.com/catalog.html" class="latest"><strong>Online Books</strong></a> | <a href="http://www.zainbooks.com/blog" class="latest"><strong>Students Blog</strong></a> | <a href="http://www.zainbooks.com/links.php" class="latest"><strong>Links</strong></a> | <a href="http://www.zainbooks.com/link_add.php" class="latest"><strong>Link Exchange</strong></a></p>
+Copyright &copy; 2010 Zain Books All Rights Reserved</td>
+  </tr>
+</table>
+
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6585282-1");
+pageTracker._trackPageview();
+} catch(err) {}</script></body>
+</html>
